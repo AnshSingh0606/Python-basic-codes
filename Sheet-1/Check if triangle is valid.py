28c1d50087340d664ab80c5f544e2743cd7e0f5f
AnshSingh0606 <ansh32511@gmail.com>
2025-08-20 08:39:09 +0530

    Add files via upload

---
+++ Sheet-1/Check if triangle is valid.py
@@ -0,0 +1,8 @@
+a = int(input("Enter angle A: "))
+b = int(input("Enter angle B: "))
+c = int(input("Enter angle C: "))
+
+if a + b + c == 180:
+    print("Valid triangle")
+else:
+    print("Invalid triangle")
