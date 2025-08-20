0f5b114a74ee69fe67cec7cdf3704f772f927402
AnshSingh0606 <ansh32511@gmail.com>
2025-08-20 09:33:47 +0530

    Update Minimum of three numbers.py

--- Sheet-1/Minimum of three numbers.py
+++ Sheet-1/Minimum of three numbers.py
@@ -1,10 +1,10 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
-
 if a <= b and a <= c:
-    print("Minimum:", a)
+    print("min:", a)
 elif b <= a and b <= c:
-    print("Minimum:", b)
+    print("min:", b)
 else:
-    print("Minimum:", c)
+    print("min:", c)
+
