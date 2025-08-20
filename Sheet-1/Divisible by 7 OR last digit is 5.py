2c4d6ecd1eb23b5c9b63c9a00b35b8e1e3ed5169
AnshSingh0606 <ansh32511@gmail.com>
2025-08-20 09:45:36 +0530

    Update Divisible by 7 OR last digit is 5.py

--- Sheet-1/Divisible by 7 OR last digit is 5.py
+++ Sheet-1/Divisible by 7 OR last digit is 5.py
@@ -1,5 +1,6 @@
 num = int(input("Enter a number: "))
 if num % 7 == 0 or num % 10 == 5:
-    print("Condition satisfied")
+    print("Good input")
 else:
-    print("Condition not satisfied")
+    print("its not divisible by 7 or last digit is not 5.")
+
