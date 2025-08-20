201eab6926d02a3a20b6936dd79744afb1d4d23d
AnshSingh0606 <ansh32511@gmail.com>
2025-08-20 23:21:42 +0530

    Update Divisible by both 5 and 11.py

--- Sheet-1/Divisible by both 5 and 11.py
+++ Sheet-1/Divisible by both 5 and 11.py
@@ -1,5 +1,6 @@
-A = int(input("Enter a number: "))
-if A % 5 == 0 and A % 11 == 0:
+a = int(input("Enter a number: "))
+if a % 5 == 0 and a % 11 == 0:
     print("Divisible by both 5 and 11")
 else:
     print("Not divisible by both 5 and 11")
+
