a7a658169509168c0183a33ad47facaaedb70b80
AnshSingh0606 <ansh32511@gmail.com>
2025-08-20 23:10:04 +0530

    Update Divisible by 3 and last digit is 4.py

--- Sheet-1/Divisible by 3 and last digit is 4.py
+++ Sheet-1/Divisible by 3 and last digit is 4.py
@@ -1,5 +1,6 @@
 a = int(input("Enter a number: "))
 if a % 3 == 0 and a % 10 == 4:
-    print("Divisible by 3 and last digit is 4")
+    print("divisible by 3 and last digit is 4")
 else:
-    print("Condition not satisfied")
+    print("incorrect input")
+
